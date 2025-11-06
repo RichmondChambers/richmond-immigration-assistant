8a6ff73d2896421dbe1a86620b919ac4bd539aaa
RichmondChambers <paul.richmond@richmondchambers.com>
2025-11-06 21:59:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import numpy as np
 import re
 import json
 import streamlit.components.v1 as components
-from markdown import markdown
+import from markdown 
+import markdown
 
 def format_for_email(response_text):
     """
